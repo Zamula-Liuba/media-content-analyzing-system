020ee4e14b991ab1f902179f101bf5bb29c01d12
Zamula-Liuba <71746212+Zamula-Liuba@users.noreply.github.com>
2021-12-30 11:12:01 +0200

    Update Mentor.js

--- src/js/models/Mentor.js
+++ src/js/models/Mentor.js
@@ -1,1 +1,32 @@
+const sequelize = require('../connection');
+const { Model, DataTypes } = require('sequelize');
+class Mentor extends Model {}
+Mentor.init(
+    {
+      id: {
+        type: DataTypes.INTEGER
+      },  
+        level:{
+            type: DataTypes.INTEGER
+        },
+        Admin_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: Admin,
+                key: 'id'
+            }
+        },  
+    },
 
+    {
+        sequelize,
+        modelName: 'Mentor',
+        timestamps: false,
+        freezeTableName: true
+    }
+);
+
+module.exports = {
+    User
+};
