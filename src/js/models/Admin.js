59c9f8f4f7147abd76a4117b8663fca7bc849899
Zamula-Liuba <71746212+Zamula-Liuba@users.noreply.github.com>
2021-12-30 11:07:28 +0200

    Update Admin.js

--- src/js/models/Admin.js
+++ src/js/models/Admin.js
@@ -1,1 +1,24 @@
+const sequelize = require('../connection');
+const { Model, DataTypes } = require('sequelize');
+class Admin extends Model {}
+User.init(
+    {
+        id: {
+            type: DataTypes.INT
+        },
+        job: {
+            type: DataTypes.STRING
+        },
+    },
 
+    {
+        sequelize,
+        modelName: 'Admin',
+        timestamps: false,
+        freezeTableName: true
+    }
+);
+
+module.exports = {
+    Admin
+};
