7a6e32ab81605352f00d3344d417ccad1fe2c8b6
Zamula-Liuba <71746212+Zamula-Liuba@users.noreply.github.com>
2021-12-30 11:18:04 +0200

    Update index.js

--- src/js/models/index.js
+++ src/js/models/index.js
@@ -1,5 +1,5 @@
 const { User } = require('./User');
-const { Expert } = require('./Expert');
+const { Mentor } = require('./Mentor');
 const { Answer } = require('./Answer');
 const { Question } = require('./Question');
 const { Alt }  = require('./Alt');
@@ -7,6 +7,8 @@ const { SelectedAlt}  = require('./SelectedAlt');
 const { Survey } = require('./Survey');
 const { SurveyAction } = require('./SurveyAction');
 const { SurveyState } = require('./SurveyState');
+const { Report } = require('./Report');
+const { UserReport } = require('./UserReport');
 
 module.exports = {
     models: {
@@ -19,6 +21,8 @@ module.exports = {
         Survey,
         SurveyAction,
         SurveyState,
+        Report,
+        UserReport,
         [Symbol.iterator]: function* () {
             const keys = Object.keys(this);
             for(const key of keys) {
