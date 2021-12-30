a7bc416e9706930b31bba9a53b368b94c4758328
Zamula-Liuba <71746212+Zamula-Liuba@users.noreply.github.com>
2021-12-30 10:44:16 +0200

    Update UserReport.js

--- src/js/models/UserReport.js
+++ src/js/models/UserReport.js
@@ -1,1 +1,44 @@
+const sequelize = require('../connection');
+const { Model, DataTypes } = require('sequelize');
+const { User } = require('./User');
+const { SelectedAlt } = require('./SelectedAlt');
+class Answer extends Model {}
+Answer.init(
+    {
+        id: {
+          type: DataTypes.INTEGER,
+        },
+        text: {
+            type: DataTypes.STRING
+        },
+        date: {
+            type: DataTypes.DATE
+        },
+        User_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: Expert,
+                key: 'id'
+            }
+        },
+        Report_date: {
+            type: DataTypes.DATE,
+            references: {
+                model: Report,
+                key: 'date'
+            }
+        }
+    },
 
+    {
+        sequelize,
+        modelName: 'UserReport',
+        timestamps: false,
+        freezeTableName: true
+    }
+);
+
+module.exports = {
+    Answer
+};
