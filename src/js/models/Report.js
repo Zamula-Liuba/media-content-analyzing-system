caf0c29a70ad492d4e292bc15a26164a59731dee
Zamula-Liuba <71746212+Zamula-Liuba@users.noreply.github.com>
2021-12-30 11:16:30 +0200

    Update Report.js

--- src/js/models/Report.js
+++ src/js/models/Report.js
@@ -1,1 +1,22 @@
+const sequelize = require('../connection');
+const { Model, DataTypes } = require('sequelize');
+const { Survey } = require('./Survey');
+class Report extends Model {}
+Report.init(
+    {
+        date: {
+            type: DataTypes.DATE
+        },
+    },
 
+    {
+        sequelize,
+        modelName: 'Report',
+        timestamps: false,
+        freezeTableName: true
+    }
+);
+
+module.exports = {
+    Report
+};
